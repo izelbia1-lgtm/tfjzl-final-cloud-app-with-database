293956dba899179b1cbfab52cf3b4cc2fbbdcffc
izelbia1-lgtm <izelbia1@gmail.com>
2026-03-06 13:51:50 +0200

    admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,29 +1,9 @@
 from django.contrib import admin
-from .models import Course, Lesson, Instructor, Learner, Enrollment, Question, Choice, Submission
-
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 2
-
-
-class QuestionInline(admin.TabularInline):
-    model = Question
-    extra = 2
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    inlines = [ChoiceInline]
-
-
-class LessonAdmin(admin.ModelAdmin):
-    list_display = ('title', 'course')
-
+from .models import Course, Lesson, Question, Choice, Enrollment, Submission
 
 admin.site.register(Course)
-admin.site.register(Lesson, LessonAdmin)
-admin.site.register(Instructor)
-admin.site.register(Learner)
-admin.site.register(Enrollment)
-admin.site.register(Question, QuestionAdmin)
+admin.site.register(Lesson)
+admin.site.register(Question)
 admin.site.register(Choice)
+admin.site.register(Enrollment)
 admin.site.register(Submission)
