46ba8dffc3bfce2a5d0e27a8184533db780f100e
izelbia1-lgtm <izelbia1@gmail.com>
2026-03-06 14:39:48 +0200

    urls.py

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -5,6 +5,22 @@ app_name = 'onlinecourse'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('course/<int:course_id>/', views.course_detail, name='course_detail'),
-    path('submit/<int:course_id>/', views.submit, name='submit'),
+
+    path(
+        'course/<int:course_id>/',
+        views.course_detail,
+        name='course_detail'
+    ),
+
+    path(
+        'submit/<int:course_id>/',
+        views.submit,
+        name='submit'
+    ),
+
+    path(
+        'course/<int:course_id>/submission/<int:submission_id>/result/',
+        views.show_exam_result,
+        name='show_exam_result'
+    ),
 ]
