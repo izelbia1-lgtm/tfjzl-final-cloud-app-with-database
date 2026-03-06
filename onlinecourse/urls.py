7c54a7073293260bedc49b0a19598ddae377718d
izelbia1-lgtm <izelbia1@gmail.com>
2026-03-06 13:53:30 +0200

    urls.py

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -1,20 +1,10 @@
 from django.urls import path
-
 from . import views
+
+app_name = 'onlinecourse'
+
 urlpatterns = [
-    # Home page
     path('', views.index, name='index'),
-
-    # User authentication
-    path('registration/', views.registration, name='registration'),
-    path('login/', views.login_request, name='login'),
-    path('logout/', views.logout_request, name='logout'),
-
-    # Course pages
-    path('<int:pk>/', views.course_details, name='course_details'),
-    path('<int:course_id>/enroll/', views.enroll, name='enroll'),
-
-    # Exam routes (required for Question 7)
-    path('<int:course_id>/exam/', views.exam, name='exam'),
-    path('<int:course_id>/submit_exam/', views.submit_exam, name='submit_exam'),
+    path('course/<int:course_id>/', views.course_detail, name='course_detail'),
+    path('submit/<int:course_id>/', views.submit, name='submit'),
 ]
