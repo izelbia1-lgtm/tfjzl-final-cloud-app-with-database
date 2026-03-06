314c53d3baf503a27762b3fc8e7640baef8199fa
izelbia1-lgtm <izelbia1@gmail.com>
2026-03-06 13:51:19 +0200

    models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -1,29 +1,47 @@
 from django.db import models
 
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 class Question(models.Model):
-    course = models.ForeignKey('Course', on_delete=models.CASCADE)
-    question_text = models.TextField()
-    grade = models.IntegerField(default=1)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
 
     def __str__(self):
         return self.question_text
 
-    def is_get_score(self, selected_choices):
-        correct_choices = self.choice_set.filter(is_correct=True)
-        if set(correct_choices) == set(selected_choices):
-            return self.grade
-        return 0
-
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=100)
+    choice_text = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
 
     def __str__(self):
         return self.choice_text
 
 
+class Enrollment(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    enrollment_date = models.DateTimeField(auto_now_add=True)
+
+
 class Submission(models.Model):
-    enrollment = models.ForeignKey('Enrollment', on_delete=models.CASCADE)
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     choices = models.ManyToManyField(Choice)
